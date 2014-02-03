b0cae7c1d0ad76bcb27750ca33609a164013cec1
AlexRuppert <sumpfkrautjunkie@googlemail.com>
2014-02-03 11:42:55 +0100

    initial setup

---
+++ Server Scripts/filterscripts/p2pInteraction.lua
@@ -0,0 +1,67 @@
+function OnFilterscriptInit()
+	print("-------------------------------------------");
+	print("Description filterscript");
+	print("-------------------------------------------");
+end
+
+function OnFilterscriptExit()
+	print("------------------------");
+	print("Removing filterscript...");
+	print("------------------------");
+end
+
+function OnPlayerChangeClass(playerid, classid)
+ 
+end
+
+function OnPlayerSelectClass(playerid, classid)
+ 
+end
+
+function OnPlayerConnect(playerid)
+ 
+end
+
+function OnPlayerDisconnect(playerid, reason)
+
+end
+
+function OnPlayerSpawn(playerid, classid)
+ 
+end
+
+function OnPlayerDeath(playerid, p_classid, killerid, k_classid)
+ 
+end
+
+function OnPlayerHit(playerid, killerid)
+ 
+end
+
+function OnPlayerUnconscious(playerid, p_classid, killerid, k_classid)
+
+end
+
+function OnPlayerText(playerid, text)
+
+end
+
+function OnPlayerCommandText(playerid, cmdtext)
+
+end
+
+function OnPlayerChangeWorld(playerid, world)
+
+end
+
+function OnPlayerEnterWorld(playerid, world)
+
+end
+
+function OnPlayerDropItem(playerid, itemid, item_instance, amount, x, y, z)
+
+end
+
+function OnPlayerTakeItem(playerid, itemid, item_instance, amount, x, y, z)
+
+end
